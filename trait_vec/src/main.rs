f12080b6b5709592cd3b805a22df0c2378ee447c
Nicolas Sabbatini <nik.code.things@gmail.com>
2023-06-04 16:03:38 -0300

    More traits

--- trait_vec/src/main.rs
+++ trait_vec/src/main.rs
@@ -1,12 +1,12 @@
 use std::{
     fmt::{Debug, Display},
-    ops::Add,
+    ops::{Add, Mul},
 };
 
 #[derive(Default, Debug)]
 struct Vec2<T>
 where
-    T: Default + Display + Debug + Add,
+    T: Default + Debug,
 {
     x: T,
     y: T,
@@ -14,16 +14,44 @@ where
 
 impl<T> Display for Vec2<T>
 where
-    T: Default + Display + Debug + Add,
+    T: Default + Debug + Display,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "(x: {}, y: {})", self.x, self.y)
     }
 }
 
+impl<T> Add for Vec2<T>
+where
+    T: Default + Debug + Add<Output = T>,
+{
+    type Output = Vec2<T>;
+
+    fn add(self, rhs: Self) -> Self::Output {
+        Vec2 {
+            x: self.x + rhs.x,
+            y: self.y + rhs.y,
+        }
+    }
+}
+
+impl<T> Mul<T> for Vec2<T>
+where
+    T: Default + Debug + Clone + Copy + Mul<Output = T>,
+{
+    type Output = Vec2<T>;
+
+    fn mul(self, rhs: T) -> Self::Output {
+        Vec2 {
+            x: self.x * rhs,
+            y: self.y * rhs,
+        }
+    }
+}
+
 trait ToVec2<T>
 where
-    T: Default + Display + Debug + Add,
+    T: Default + Debug + Add,
 {
     fn to_vec2(&self) -> Vec2<T>;
 }
@@ -35,21 +63,26 @@ impl ToVec2<usize> for usize {
 }
 
 fn main() {
+    let vec_str = Vec2 {
+        x: "Holi!",
+        y: "Chau!",
+    };
+    println!("{vec_str:?}");
+    println!("{vec_str}\n");
+
     let vec1: Vec2<usize> = Vec2::default();
-
     println!("{vec1:?}");
-    println!("{vec1}");
-
-    // let vec_str = Vec2 {
-    //     x: "Holi!",
-    //     y: "Chau!",
-    // };
-    //
-    // println!("{vec_str:?}");
-    // println!("{vec_str}");
+    println!("{vec1}\n");
 
     let vec2 = 10.to_vec2();
-
     println!("{vec2:?}");
-    println!("{vec2}");
+    println!("{vec2}\n");
+
+    let vec_sum = vec1 + vec2;
+    println!("{vec_sum:?}");
+    println!("{vec_sum}\n");
+
+    let vec_mul = vec_sum * 2;
+    println!("{vec_mul:?}");
+    println!("{vec_mul}\n");
 }

eb4b6aae3bf7130bced2a07b3cde787258b604bc
Nicolas Sabbatini <nik.code.things@gmail.com>
2023-06-04 15:39:36 -0300

    finish trait_vect

--- trait_vec/src/main.rs
+++ trait_vec/src/main.rs
@@ -1,17 +1,36 @@
-use std::fmt::Display;
+use std::{
+    fmt::{Debug, Display},
+    ops::Add,
+};
 
 #[derive(Default, Debug)]
-struct Vec2<T> {
+struct Vec2<T>
+where
+    T: Default + Display + Debug + Add,
+{
     x: T,
     y: T,
 }
 
 impl<T> Display for Vec2<T>
 where
-    T: Display,
+    T: Default + Display + Debug + Add,
 {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
         write!(f, "(x: {}, y: {})", self.x, self.y)
+    }
+}
+
+trait ToVec2<T>
+where
+    T: Default + Display + Debug + Add,
+{
+    fn to_vec2(&self) -> Vec2<T>;
+}
+
+impl ToVec2<usize> for usize {
+    fn to_vec2(&self) -> Vec2<usize> {
+        Vec2 { x: *self, y: *self }
     }
 }
 
@@ -21,11 +40,16 @@ fn main() {
     println!("{vec1:?}");
     println!("{vec1}");
 
-    let vec_str = Vec2 {
-        x: "Holi!",
-        y: "Chau!",
-    };
+    // let vec_str = Vec2 {
+    //     x: "Holi!",
+    //     y: "Chau!",
+    // };
+    //
+    // println!("{vec_str:?}");
+    // println!("{vec_str}");
 
-    println!("{vec_str:?}");
-    println!("{vec_str}");
+    let vec2 = 10.to_vec2();
+
+    println!("{vec2:?}");
+    println!("{vec2}");
 }
